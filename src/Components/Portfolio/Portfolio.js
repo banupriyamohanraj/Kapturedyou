ed0f767997e9425e321502f00bfccd8513ee2832
banupriyamohanraj <banupriyamohanrajoffl@gmail.com>
2021-09-13 17:51:01 +0530

    fixed broken links

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -9,13 +9,13 @@ export default function Portfolio() {
 
         <div className="Portfolio__container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <img src={Kaptured} alt="logo" height="100px" width="150px" ></img>
+        <img src={Kaptured} alt="logo" height="100px" width="150px" ></img>&emsp;
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <Link to="About" smooth={true} duration={500} activeClass="active" offset={-70}>ABOUT <span class="sr-only">(current)</span></Link>
+                        <Link to="About" smooth={true} duration={500} activeClass="active" offset={-70}>ABOUT <span class="sr-only">(current)</span></Link>&emsp;
                         <Link to="Myportfolio" smooth={true} duration={500} offset={-70}> PORTFOLIO</Link>
 
 
