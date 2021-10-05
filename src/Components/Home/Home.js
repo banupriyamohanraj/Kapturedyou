8a352bcbb838afe1148e7c9b20ff75fd5fc76a3f
banupriyamohanraj <banupriyamohanrajoffl@gmail.com>
2021-10-05 20:16:45 +0530

    made it responsive

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,11 +10,11 @@ export default function Home() {
             <div className="row m-0">
                 <div className=" col-6 Home__subTitle">
                     <div className="quote">
-                     <Fade left> <p className="title1">SMILE FOR<br /> A <span style={{color:"orange"}}>MOMENT</span>,<br /> LET ME <br /><span style={{color:"orange"}}>KAPTURE</span> YOU </p>
+                     <Fade left> <p className="title1">SMILE FOR<br /> A <span style={{color:"orangered"}}>MOMENT</span>,<br /> LET ME <br /><span style={{color:"orangered"}}>KAPTURE</span> YOU </p>
                     
-                        <p classname="title2" style={{color:"orange"}}>Fashion | Event Photography </p><br />
+                        <p classname="title2" style={{color:"orangered"}}>Fashion | Event Photography </p><br />
 
-<Link to="/portfolio"><button type="button" className="btn btn-secondary">PORTFOLIO</button></Link></Fade>
+<Link to="/portfolio"><button type="button" className="btn btn-ssecondary">PORTFOLIO</button></Link></Fade>
 
                     </div>
                 </div>
